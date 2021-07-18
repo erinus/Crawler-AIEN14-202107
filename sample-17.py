1399261d25d7b17474ecda390446d8f9449945b9
erinus <erinus.startup@gmail.com>
2021-07-18 14:32:35 +0800

    update

--- sample-17.py
+++ sample-17.py
@@ -1,11 +1,16 @@
+import urllib.parse
+
 import requests
 import pyquery
 
-def GetIndex():
+cookie = '<請輸入自己的 Cookie>'
+
+def CheckStatus():
+    global cookie
     response = requests.get(
         url='https://shopee.tw/api/v2/user/account_info?from_wallet=false&skip_address=1&need_cart=1',
         headers={
-            'cookie': '<請輸入自己在瀏覽器上登入後的 Cookie>',
+            'cookie': cookie,
             'referer': 'https://shopee.tw/',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'x-api-source': 'pc',
@@ -18,7 +23,32 @@ def GetIndex():
         print('NOT 200')
         return
 
-    print(response.text)
+    return response.json()['error'] == 0
+
+def SearchProducts(keyword):
+    global cookie
+    response = requests.get(
+        url=f'https://shopee.tw/api/v4/search/search_items?by=relevancy&keyword={urllib.parse.quote(keyword)}&limit=60&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2',
+        headers={
+            'cookie': cookie,
+            'referer': 'https://shopee.tw/',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'x-api-source': 'pc',
+            'x-csrftoken': 'NCaZXYtwXQOI7pth3q74h4msPjfYcsd6',
+            'x-requested-with': 'XMLHttpRequest',
+            'x-shopee-language': 'zh-Hant'
+        }
+    )
+    if response.status_code != 200:
+        print('NOT 200')
+        return
+
+    print(response.json())
 
 if __name__ == '__main__':
-    GetIndex()
+    res = CheckStatus()
+    if res:
+        print('登入成功')
+        SearchProducts('iphone')
+    else:
+        print('登入失敗，請更換新的 Cookie')
