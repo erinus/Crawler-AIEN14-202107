113571b8b4f1d3aa5c60944aa2e71b536fc56040
erinus <erinus.startup@gmail.com>
2021-07-18 11:29:58 +0800

    update

--- sample-16.py
+++ sample-16.py
@@ -9,7 +9,7 @@ def GetPost(url):
     response = requests.get(
         url=url,
         headers={
-            'referer': f'https://www.google.com/',
+            'referer': f'https://www.coolaler.com/reviews/',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
     )
@@ -17,7 +17,7 @@ def GetPost(url):
         print('NOT 200')
         return
 
-    with open('apple.html', 'wb') as f:
+    with open('coolar.html', 'wb') as f:
         f.write(response.content)
 
     doc = pyquery.PyQuery(response.text)
@@ -43,8 +43,10 @@ def GetPostList():
 
     for elm_div_porta_article_item in elms_div_porta_article_item:
         elm_a = elm_div_porta_article_item('a.porta-article-header a')
-        print(elm_a.text())
-
+        url = elm_a.attr('href')
+        url = f'https://www.coolaler.com{url}'
+        GetPost(url)
+        time.sleep(1)
 
 if __name__ == '__main__':
     GetPostList()
