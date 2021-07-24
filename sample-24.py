3edf0922af51b58621cdcee8db8bd470efdc1cc7
erinus <erinus.startup@gmail.com>
2021-07-24 16:12:04 +0800

    update

---
+++ sample-24.py
@@ -0,0 +1,46 @@
+import requests
+import pyquery
+
+def GetData(proxies):
+    for proxy in proxies:
+        try:
+            response = requests.get('<URL>', proxies=proxy, timeout=5)
+            if response.status_code != 200:
+                print('NOT 200')
+                continue
+            print(response.text)
+        except:
+            continue
+        break
+
+def GetProxies():
+    response = requests.get('https://free-proxy-list.net/')
+    if response.status_code != 200:
+        print('NOT 200')
+        return
+
+    doc = pyquery.PyQuery(response.text)
+    elm_textarea = doc('textarea')
+    txt_textarea = elm_textarea.text()
+
+    lines = txt_textarea.split('\n')
+    lines = lines[3:-1]
+    # txt_textarea = '\n'.join(lines)
+    # print(txt_textarea)
+
+    proxies = []
+    for line in lines:
+        http_proxy = f'http://{line}'
+        https_proxy = f'https://{line}'
+        # print(f'HTTP: {http_proxy}')
+        # print(f'HTTPS: {https_proxy}')
+        # print('')
+        proxies.append({
+            'http': http_proxy,
+            'https': https_proxy
+        })
+
+    GetData(proxies)
+
+if __name__ == '__main__':
+    GetProxies()
