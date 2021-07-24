85e01a88fafbd11a7f815e90800a55e45ce1fd78
erinus <erinus.startup@gmail.com>
2021-07-24 10:48:21 +0800

    update

--- sample-19.py
+++ sample-19.py
@@ -4,14 +4,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 def TestChrome():
+    # mobile_emulation = { 'deviceName': 'Google Nexus 5' }
+
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    options.add_argument('--kiosk')
+    # options.add_experimental_option('mobileEmulation', mobile_emulation)
+    # options.add_argument('--kiosk')
 
     driver = webdriver.Chrome(chrome_options=options)
     driver.get('https://www.facebook.com/')
 
-    time.sleep(15)
+    # elm_input_email = driver.find_element_by_id('email')
+    elm_input_email = driver.find_element_by_css_selector('#email')
+    elm_input_email.send_keys('<自己的帳號>')
+
+    # elm_input_pass = driver.find_element_by_id('pass')
+    elm_input_pass = driver.find_element_by_css_selector('#pass')
+    elm_input_pass.send_keys('<自己的密碼>')
+
+    elm_button_login = driver.find_element_by_css_selector('button[name="login"]')
+    elm_button_login.click()
+
+    time.sleep(60)
 
     driver.quit()
 
